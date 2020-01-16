cc94d4871a170bdf4141071172acdca5f91f78c9
withteja <csaiteja533@gmail.com>
2020-01-15 19:55:13 -0600

    Final

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Helo, World!\n";
     return 0;
 }
